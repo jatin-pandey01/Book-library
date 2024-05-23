2ed3607ccb403c55dfc14df3d77d0671bff88ce4
jatin-pandey01 <=>
2024-05-23 11:45:20 +0530

    Final commit

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -16,8 +16,8 @@ const About = () => {
           </div>
           <div className='about-text'>
             <h2 className='about-title fs-26 ls-1'>About BookHub</h2>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus dignissimos consequuntur vero commodi provident maiores, iusto atque corrupti voluptate vel sequi consectetur unde aliquam corporis saepe animi non, tempora reiciendis molestias sed laudantium dolores. Assumenda aperiam fuga quo voluptate commodi ullam aliquam expedita voluptas delectus.</p>
-            <p className='fs-17'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, dicta, possimus inventore eveniet atque voluptatibus repellendus aspernatur illo aliquam dignissimos illum. Commodi, porro omnis dolore amet neque modi quas eum!</p>
+            <p className='fs-17'>Welcome to our Library Project! Our mission is to create a dynamic community space that fosters a love for reading and lifelong learning. We aim to provide accessible resources and a welcoming environment where people of all ages and backgrounds can explore knowledge, share ideas, and connect with one another. Our library will feature an extensive collection of books, digital resources, and educational materials, ensuring that everyone has the opportunity to learn and grow.</p>
+            <p className='fs-17'>We are committed to supporting the educational and cultural needs of our community through various programs, workshops, and events. By offering a wide range of activities, from author talks and reading groups to educational workshops and cultural celebrations, we strive to enrich the lives of our patrons. Our library will also provide dedicated spaces for children, quiet study areas, and access to technology, making it a hub for inspiration, creativity, and personal development.</p>
           </div>
         </div>
       </div>
